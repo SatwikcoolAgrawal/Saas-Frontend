85a99e118a9ff614da3ece6aa7cbd2c64580d9ab
knehaa <nehagaikwad275@gmail.com>
2023-11-16 23:45:49 +0530

    navbar added with logout and isadmin feature

---
+++ src/layouts/UserLayout.jsx
@@ -0,0 +1,214 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+import AdbIcon from '@mui/icons-material/Adb';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { decodeToken } from "react-jwt";
+
+const settings = ['Dashboard', 'Logout'];
+
+function UserLayout({ children }) {
+
+
+    const [isadmin, setIsadmin] = useState(false);
+    const [isLogin, setIslogin] = useState(false);
+
+
+    useEffect(() => {
+
+
+        const token = sessionStorage.getItem('access-token');
+
+        console.log(token);
+
+        if (token) {
+            setIslogin(true);
+            const data = decodeToken(token);
+            //token decode
+            console.log(data);
+
+            const { isAdmin, isSuperAdmin } = data;
+
+            // check for isadmin or issuperAdmin
+
+            setIsadmin(isAdmin || isSuperAdmin);
+
+        }
+
+
+
+
+
+    }, [])
+    const [anchorElNav, setAnchorElNav] = React.useState(null);
+    const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+    const handleOpenNavMenu = (event) => {
+        setAnchorElNav(event.currentTarget);
+    };
+    const handleOpenUserMenu = (event) => {
+        setAnchorElUser(event.currentTarget);
+    };
+
+    const handleCloseNavMenu = () => {
+        setAnchorElNav(null);
+    };
+
+    const handleCloseUserMenu = () => {
+        setAnchorElUser(null);
+    };
+
+    return (
+        <>
+            <AppBar position="static">
+                <Container maxWidth="xl">
+                    <Toolbar >
+                        <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+                        <Typography
+                            variant="h6"
+                            noWrap
+                            component="a"
+                            href="#app-bar-with-responsive-menu"
+                            sx={{
+                                mr: 0,
+                                display: { xs: 'none', md: 'flex' },
+                                fontFamily: 'monospace',
+                                fontWeight: 700,
+                                letterSpacing: '.3rem',
+                                color: 'inherit',
+                                textDecoration: 'none',
+                            }}
+                        >
+                            SAAS
+                        </Typography>
+
+                        {/* <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+                        <IconButton
+                            size="large"
+                            aria-label="account of current user"
+                            aria-controls="menu-appbar"
+                            aria-haspopup="true"
+                            onClick={handleOpenNavMenu}
+                            color="inherit"
+                        >
+                            <MenuIcon />
+                        </IconButton>
+                        <Menu
+                            id="menu-appbar"
+                            anchorEl={anchorElNav}
+                            anchorOrigin={{
+                                vertical: 'bottom',
+                                horizontal: 'left',
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'left',
+                            }}
+                            open={Boolean(anchorElNav)}
+                            onClose={handleCloseNavMenu}
+                            sx={{
+                                display: { xs: 'block', md: 'none' },
+                            }}
+                        >
+                             {pages.map((page) => (
+                                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                                    <Typography textAlign="center">{page}</Typography>
+                                </MenuItem>
+                            ))} 
+                        </Menu>
+                    </Box> */}
+                        <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
+                        <Typography
+                            variant="h5"
+                            noWrap
+                            component="a"
+
+                            sx={{
+                                mr: 2,
+                                display: { xs: 'flex', md: 'none' },
+                                flexGrow: 1,
+                                fontFamily: 'monospace',
+                                fontWeight: 700,
+                                letterSpacing: '.3rem',
+                                color: 'inherit',
+                                textDecoration: 'none',
+                            }}
+                        >
+                            LOGO
+                        </Typography>
+                        <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+
+                        </Box>
+
+                        {isLogin ? <Box sx={{ flexGrow: 0 }}>
+                            <Tooltip title="Open settings" >
+                                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                                    <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+                                </IconButton>
+                            </Tooltip>
+                            <Menu
+                                sx={{ mt: '45px' }}
+                                id="menu-appbar"
+                                anchorEl={anchorElUser}
+                                anchorOrigin={{
+                                    vertical: 'top',
+                                    horizontal: 'right',
+                                }}
+                                keepMounted
+                                transformOrigin={{
+                                    vertical: 'top',
+                                    horizontal: 'right',
+                                }}
+                                open={Boolean(anchorElUser)}
+                                onClose={handleCloseUserMenu}
+                            >
+
+                                {isadmin && <MenuItem >
+                                    <Typography textAlign="center">Dashboard</Typography>
+                                </MenuItem>}
+                                <MenuItem >
+                                    <Typography onClick={() => {
+                                        sessionStorage.removeItem('access-token');
+                                        // navigate("/");
+
+                                    }} textAlign="center">Log out</Typography>
+                                </MenuItem>
+
+                            </Menu>
+                        </Box> : <Typography
+                            variant="h6"
+                            noWrap
+                            component="a"
+                            href="#app-bar-with-responsive-menu"
+                            sx={{
+                                mr: 0,
+                                display: { xs: 'none', md: 'flex' },
+                                fontFamily: 'monospace',
+                                fontWeight: 700,
+                                letterSpacing: '.3rem',
+                                color: 'inherit',
+                                textDecoration: 'none',
+                            }}
+                        >
+                            Log In
+                        </Typography>}
+                    </Toolbar>
+                </Container>
+            </AppBar>
+            {children}
+        </>
+    );
+}
+export default UserLayout;
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -3,9 +3,10 @@ import { Outlet, Navigate, useRoutes } from 'react-router-dom';
 
 // import DashboardLayout from 'src/layouts/dashboard';
 import Navbar from '../components/NavBar';
-export const Home  = lazy(() => import('../pages/Plans'));
-export const Product = lazy(() => import('../pages/Product'));
-export const Login = lazy(() => import('../pages/LoginSignup'));
+import UserLayout from '../layouts/UserLayout';
+import Home from '../pages/Plans';
+import Product from '../pages/Product';
+import Login from '../pages/LoginSignup';
 // export const LoginPage = lazy(() => import('src/pages/login'));
 // export const ProductsPage = lazy(() => import('src/pages/products'));
 // export const Page404 = lazy(() => import('src/pages/page-not-found'));
@@ -13,55 +14,53 @@ export const Login = lazy(() => import('../pages/LoginSignup'));
 // ----------------------------------------------------------------------
 
 export default function Router() {
-  const routes = useRoutes([
-    {
-        path:"/",
-        element:(
-        
-       
-            <Suspense >
-                {/* <UserLayout> */}
-                <Navbar/>
-                    <Outlet/>
-                {/* </UserLayout> */}
-            </Suspense>
-            
-        ),
-        children: [
-            {element:<Navigate to="/home"/>,index:true},
-            {path:"/home",element:<Home/>},
-            {path:'/plans/:service',element:<Product/>},
-           
-            // {path:"/cart",element:<Cart/>},
-        ]
-    },
-    {path:"/login",element:<Login/>},
-    // {path:"/signup",element:<Signup/>},
-    // {path:"/checkout",element:<Checkout/>},
-    // {
-    //   element: (
-    //     <DashboardLayout>
-    //       <Suspense>
-    //         <Outlet />
-    //       </Suspense>
-    //     </DashboardLayout>
-    //   ),
-    //   children: [
-    //     { element: <IndexPage />, index: true },
-    //     { path: 'user', element: <UserPage /> },
-    //     { path: 'products', element: <ProductsPage /> },
-    //     { path: 'blog', element: <BlogPage /> },
-    //   ],
-    // },
-    // {
-    //   path: '/404',
-    //   element: <Page404 />,
-    // },
-    // {
-    //   path: '*',
-    //   element: <Navigate to="/home" replace />,
-    // },
-  ]);
+    const routes = useRoutes([
+        {
+            path: "/",
+            element: (
 
-  return routes;
+
+
+                <UserLayout>
+
+                    <Outlet />
+                </UserLayout>
+            ),
+            children: [
+                { element: <Navigate to="/home" />, index: true },
+                { path: "/home", element: <Home /> },
+                { path: '/plans/:service', element: <Product /> },
+
+                // {path:"/cart",element:<Cart/>},
+            ]
+        },
+        { path: "/login", element: <Login /> },
+        // {path:"/signup",element:<Signup/>},
+        // {path:"/checkout",element:<Checkout/>},
+        // {
+        //   element: (
+        //     <DashboardLayout>
+        //       <Suspense>
+        //         <Outlet />
+        //       </Suspense>
+        //     </DashboardLayout>
+        //   ),
+        //   children: [
+        //     { element: <IndexPage />, index: true },
+        //     { path: 'user', element: <UserPage /> },
+        //     { path: 'products', element: <ProductsPage /> },
+        //     { path: 'blog', element: <BlogPage /> },
+        //   ],
+        // },
+        // {
+        //   path: '/404',
+        //   element: <Page404 />,
+        // },
+        // {
+        //   path: '*',
+        //   element: <Navigate to="/home" replace />,
+        // },
+    ]);
+
+    return routes;
 }
